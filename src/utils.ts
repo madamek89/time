c32d040628eeb4a30dd2337fdd9506bad7f98342
Adam Rutkowski <a_rutkowski@outlook.com>
2018-01-23 21:41:23 +0100

    chore: mark functions as private

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
+/** @private */
 export function mod(n: number, m: number) {
   return ((n % m) + m) % m;
 }
 
+/** @private */
 export function pad(n: number): string {
   return ("0" + n.toString()).slice(-2);
 }
